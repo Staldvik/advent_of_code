3d2ffe7ccc803e2ecbedc4e0ce28e703ee4aa001
Staldvik <aasmund.staldvik@gmail.com>
2023-03-22 20:56:04 +0100

    rust - day 5 cleanup

--- rust/day5/src/main.rs
+++ rust/day5/src/main.rs
@@ -1,27 +1,11 @@
 use std::{fs::read_to_string, str::FromStr};
 
-#[derive(Debug)]
-struct Stack {
-    stack: Vec<char>,
-}
+type Stack = Vec<char>;
+type Moves = Vec<Move>;
 
-impl Stack {
-    fn new() -> Self {
-        Stack { stack: Vec::new() }
-    }
-
-    fn top_crate(&self) -> Option<&char> {
-        self.stack.last()
-    }
-
-    fn place_crates(&mut self, crates: &[char]) {
-        self.stack.extend_from_slice(crates)
-    }
-
-    fn lift(&mut self, n: usize) -> Vec<char> {
-        let split_point = self.stack.len() - n;
-        self.stack.split_off(split_point)
-    }
+enum CrateMover {
+    V9000,
+    V9001,
 }
 
 #[derive(Debug)]
@@ -30,18 +14,24 @@ struct StorageDepot {
 }
 
 impl StorageDepot {
-    fn new() -> Self {
-        Self { stacks: Vec::new() }
+    fn get_top_row(&self) -> String {
+        let mut result = String::new();
+        for char in self.stacks.iter().filter_map(|s| s.last()) {
+            result.push_str(&char.to_string());
+        }
+        return result;
     }
 
-    fn print(&self) {
-        let answer: Vec<&char> = self
-            .stacks
-            .iter()
-            .map(|stack| stack.top_crate().unwrap_or(&' '))
-            .collect();
+    fn move_crates(&mut self, moves: Vec<Move>, mover: CrateMover) {
+        for Move { amount, from, to } in moves {
+            let stack = self.stacks.get_mut(from - 1).unwrap();
+            let crates_to_move = stack.split_off(stack.len() - amount);
 
-        println!("Answer is {:?}", answer)
+            match mover {
+                CrateMover::V9000 => self.stacks[to - 1].extend(crates_to_move.iter().rev()),
+                CrateMover::V9001 => self.stacks[to - 1].extend(crates_to_move.iter()),
+            }
+        }
     }
 }
 
@@ -49,27 +39,27 @@ impl FromStr for StorageDepot {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut storage_depot: StorageDepot = StorageDepot::new();
-        let stacks = &mut storage_depot.stacks;
+        let mut stacks: Vec<Stack> = Vec::new();
 
-        for row in s.lines().rev() {
-            for (offset, c) in row.char_indices().step_by(4) {
-                if c == '[' {
-                    let stack_index = if offset == 0 { 0 } else { offset / 4 };
-                    let next_crate = row.chars().nth(offset + 1).unwrap();
-                    match stacks.get_mut(stack_index) {
-                        Some(stack) => stack.place_crates(&[next_crate]),
-                        None => {
-                            let mut new_stack = Stack::new();
-                            new_stack.place_crates(&[next_crate]);
-                            stacks.push(new_stack);
-                        }
+        for line in s.lines().rev() {
+            for (offset, c) in line.char_indices().skip(1).step_by(4) {
+                if c == ' ' {
+                    continue;
+                }
+
+                let stack_index = offset / 4;
+                match stacks.get_mut(stack_index) {
+                    Some(stack) => stack.push(c),
+                    None => {
+                        let mut new_stack = Stack::new();
+                        new_stack.push(c);
+                        stacks.push(new_stack);
                     }
                 }
             }
         }
 
-        Ok(storage_depot)
+        return Ok(StorageDepot { stacks });
     }
 }
 
@@ -83,47 +73,52 @@ impl FromStr for Move {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let instructions: Vec<_> = s
+        let instructions: Vec<usize> = s
             .split_whitespace()
             .filter_map(|s| s.parse::<usize>().ok())
             .collect();
 
-        match instructions[0..=2] {
-            [amount, from, to] => Ok(Self { amount, from, to }),
-            _ => panic!("Malformed instructions?"),
-        }
+        return match instructions[0..=2] {
+            [amount, from, to] => Ok(Move { amount, from, to }),
+            _ => Err("Malformed instructions?".to_string()),
+        };
     }
+}
+
+fn parse_input(input: &String) -> (StorageDepot, Moves) {
+    let (crates, instructions) = input
+        .split_once("\n\n")
+        .expect("Couldn't split input. No double newline found");
+
+    let storage_depot = crates.parse::<StorageDepot>().expect(&format!(
+        "Couldn't parse crates into StorageDepot: {crates}"
+    ));
+
+    let moves: Moves = instructions
+        .lines()
+        .map(|line| {
+            line.parse::<Move>()
+                .expect(&format!("Couldn't parse instruction line into Move {line}"))
+        })
+        .collect();
+
+    return (storage_depot, moves);
+}
+
+fn part_1(input: &String) {
+    let (mut storage_depot, moves) = parse_input(input);
+    storage_depot.move_crates(moves, CrateMover::V9000);
+    println!("Part 1 answer is: {:?}", storage_depot.get_top_row());
+}
+
+fn part_2(input: &String) {
+    let (mut storage_depot, moves) = parse_input(input);
+    storage_depot.move_crates(moves, CrateMover::V9001);
+    println!("Part 2 answer is: {:?}", storage_depot.get_top_row());
 }
 
 fn main() {
     let input = read_to_string("src/input.txt").unwrap();
-
-    let (crates, instructions) = input
-        .split_once("\n\n")
-        .expect("Malformed input? No double newline found");
-
-    let mut storage_depot = crates
-        .parse::<StorageDepot>()
-        .expect("Couldn't parse crates into StorageDepot");
-
-    let stacks = &mut storage_depot.stacks;
-
-    for row in instructions.lines() {
-        let current_move = row
-            .parse::<Move>()
-            .expect("Couldn't parse instruction into Move");
-
-        let Move { amount, from, to } = current_move;
-
-        for _ in 0..amount {
-            if let Some(crate_to_move) = stacks.get_mut(from - 1).unwrap().lift(1).last() {
-                stacks
-                    .get_mut(to - 1)
-                    .unwrap()
-                    .place_crates(&[*crate_to_move]);
-            }
-        }
-    }
-
-    storage_depot.print();
+    part_1(&input);
+    part_2(&input);
 }
