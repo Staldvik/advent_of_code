eeb46509f8dc6d08b69184c9351c6e20246ca5d9
Staldvik <aasmund.staldvik@gmail.com>
2023-12-11 09:27:21 +0100

    don't know why part2 isn't working

--- 2023/ts/day11/index.ts
+++ 2023/ts/day11/index.ts
@@ -1,4 +1,4 @@
-import { getInputFile, getTestFile, readFile, testSolution } from "../utils";
+import { getInputFile, getTestFile, testSolution } from "../utils";
 
 const testFile = getTestFile(__dirname);
 const inputFile = getInputFile(__dirname);
@@ -7,37 +7,52 @@ type Pos = {
   x: number;
   y: number;
   name: string;
+  hCrossed: number[];
+  vCrossed: number[];
 };
 
-const expandAndGetGalaxies = (input: string) => {
+const expandAndGetGalaxies = (input: string, boundaryCost = 1) => {
   let rows = input.split("\n");
+  const vBoundaries: number[] = [];
+  const hBoundaries: number[] = [];
 
   // expand columns
   for (let colI = 0; colI < rows[0].length; colI++) {
-    const isEmptyCol = rows.every((row) => row[colI] === ".");
+    const isEmptyCol = rows.every((row) => row[colI] !== "#");
     if (isEmptyCol) {
-      rows = rows.map((row) => row.slice(0, colI) + "." + row.slice(colI));
-      colI++;
+      vBoundaries.push(colI);
     }
   }
 
   // expand rows
-  rows = rows.flatMap((row) => {
+  for (let rowI = 0; rowI < rows.length; rowI++) {
+    const row = rows[rowI];
     const isEmptyRow = !row.includes("#");
-    if (isEmptyRow) return [row, row];
-    return row;
-  });
+    if (isEmptyRow) {
+      hBoundaries.push(rowI);
+    }
+  }
 
   const galaxies: Pos[] = [];
+
   rows.forEach((row, rowI) => {
     for (let colI = 0; colI < row.length; colI++) {
       const char = row[colI];
-      if (char === "#")
+      if (char === "#") {
+        const galaxyX = colI;
+        const galaxyY = rowI;
+
+        const hCrossed = hBoundaries.filter((yBoundary) => galaxyY > yBoundary);
+        const vCrossed = vBoundaries.filter((xBoundary) => galaxyX > xBoundary);
+
         galaxies.push({
-          x: colI,
-          y: rowI,
+          x: galaxyX + vCrossed.length * boundaryCost,
+          y: galaxyY + hCrossed.length * boundaryCost,
           name: `Galaxy #${galaxies.length + 1}`,
+          hCrossed,
+          vCrossed,
         });
+      }
     }
   });
 
@@ -61,9 +76,26 @@ const part1 = (input: string) => {
   return sum / 2;
 };
 
-const part2 = (input: string) => {};
+const part2 = (input: string) => {
+  const galaxies = expandAndGetGalaxies(input, 10);
+  // const galaxies = expandAndGetGalaxies(input, 1_000_000);
+
+  let sum = 0;
+  for (const galaxy of galaxies) {
+    const restOfGalaxies = galaxies.filter((pos) => pos.name !== galaxy.name);
+    sum += restOfGalaxies.reduce((acc, curr) => {
+      const distance =
+        Math.abs(galaxy.y - curr.y) + Math.abs(galaxy.x - curr.x);
+
+      return acc + distance;
+    }, 0);
+  }
+
+  return sum / 2;
+};
 
 // testSolution("374", part1, testFile);
+testSolution("1030", part2, testFile);
 
-console.log("Part 1:", part1(inputFile));
-// console.log("Part 2:", part2(inputFile));
+// console.log("Part 1:", part1(inputFile));
+// console.log("Part 2:", part2(inputFile)); // 504715573144 (too high)
