6b57e639539c4b9240dea767ce894763918e72fd
Staldvik <aasmund.staldvik@gmail.com>
2024-06-08 10:47:45 +0200

    2021 - day1 - ts

--- 2021/ts/day1/index.ts
+++ 2021/ts/day1/index.ts
@@ -1,19 +1,49 @@
-import { getTestFile, testSolution } from "../utils";
+import { getInputFile, getTestFile, testSolution } from "../utils";
 
 const testFile = getTestFile(__dirname);
+const inputFile = getInputFile(__dirname);
 
 const part1 = (input: string) => {
-  return input.split("\n").reduce(
-    ({ increasedTimes, previousDepth }, currentLine) => {
+  const { increasedTimes } = input.split("\n").reduce(
+    (acc, currentLine) => {
       const currentDepth = parseInt(currentLine, 10);
       const newIncreasedTimes =
-        previousDepth && currentDepth > previousDepth
-          ? increasedTimes + 1
-          : increasedTimes;
-      return { increasedTimes: newIncreasedTimes, previousDepth: currentDepth };
+        acc.previousDepth && currentDepth > acc.previousDepth
+          ? acc.increasedTimes + 1
+          : acc.increasedTimes;
+      acc.increasedTimes = newIncreasedTimes;
+      acc.previousDepth = currentDepth;
+      return acc;
     },
     { increasedTimes: 0, previousDepth: 0 }
-  ).increasedTimes;
+  );
+
+  return increasedTimes;
+};
+
+const part2 = (input: string) => {
+  const { increasedTimes } = input.split("\n").reduce(
+    (acc, currentLine, i, arr) => {
+      const window = arr
+        .slice(i, i + 3)
+        .reduce((sum, c) => sum + parseInt(c, 10), 0);
+
+      const newIncreasedTimes =
+        acc.window && window > acc.window
+          ? acc.increasedTimes + 1
+          : acc.increasedTimes;
+      acc.increasedTimes = newIncreasedTimes;
+      acc.window = window;
+      return acc;
+    },
+    { increasedTimes: 0, window: 0 }
+  );
+
+  return increasedTimes;
 };
 
 testSolution("7", part1, testFile);
+testSolution("1557", part1, inputFile);
+
+testSolution("5", part2, testFile);
+testSolution("1608", part2, inputFile);
