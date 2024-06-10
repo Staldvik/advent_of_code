89ba09d09e75b8db432436476ef35846aa2074d2
Staldvik <aasmund.staldvik@gmail.com>
2024-06-10 15:26:20 +0200

    add day-template

---
+++ 2021/ts/template/index.ts
@@ -0,0 +1,15 @@
+import { clearLine } from "readline";
+import { getInputFile, getTestFile, testSolution } from "../utils";
+
+const testFile = getTestFile(__dirname);
+const inputFile = getInputFile(__dirname);
+
+const part1 = (input: string) => {};
+
+const part2 = (input: string) => {};
+
+testSolution("5", part1, testFile);
+// testSolution("6548", part1, inputFile);
+
+// testSolution("12", part2, testFile);
+// testSolution("19663", part2, inputFile); // 19633 = Too low
