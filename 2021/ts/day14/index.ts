accaa56841359b45df60015e438f0c01741dbc4a
Staldvik <aasmund.staldvik@gmail.com>
2024-07-26 20:48:10 +0200

    2021 - day14 - ts - part 2

--- 2021/ts/day14/index.ts
+++ 2021/ts/day14/index.ts
@@ -11,7 +11,6 @@ type Insertion = {
 
 const part1 = (input: string) => {
   const [template, insertionsString] = input.split("\n\n");
-  console.log("🚀 ~ part1 ~ template:", template);
   const insertions: Insertion[] = insertionsString.split("\n").map((l) => {
     const [pair, result] = l.split(" -> ");
     return {
@@ -45,10 +44,58 @@ const part1 = (input: string) => {
   return Math.max(...countValues) - Math.min(...countValues);
 };
 
-const part2 = (input: string) => {};
+const part2 = (input: string) => {
+  const [template, insertionsString] = input.split("\n\n");
+
+  const insertions: Insertion[] = insertionsString.split("\n").map((l) => {
+    const [pair, result] = l.split(" -> ");
+    return {
+      pair,
+      result,
+    };
+  });
+
+  const letterCount: Record<string, number> = {};
+  for (const letter of template) {
+    if (!letterCount[letter]) letterCount[letter] = 0;
+    letterCount[letter] += 1;
+  }
+
+  let pairs: Record<string, number> = {};
+  for (let i = 1; i < template.length; i++) {
+    const pair = template[i - 1] + template[i];
+    if (!pairs[pair]) pairs[pair] = 0;
+    pairs[pair] += 1;
+  }
+
+  for (let step = 0; step < 40; step++) {
+    const currentPairs: typeof pairs = {};
+
+    Object.entries(pairs).forEach(([pair, count]) => {
+      const match = insertions.find((i) => i.pair === pair);
+      if (match) {
+        const newPair1 = `${pair[0]}${match.result}`;
+        const newPair2 = `${match.result}${pair[1]}`;
+        if (!currentPairs[newPair1]) currentPairs[newPair1] = 0;
+        if (!currentPairs[newPair2]) currentPairs[newPair2] = 0;
+        if (!letterCount[match.result]) letterCount[match.result] = 0;
+        currentPairs[newPair1] += count;
+        currentPairs[newPair2] += count;
+        letterCount[match.result] += count;
+      }
+    });
+
+    pairs = currentPairs;
+  }
+
+  return (
+    Math.max(...Object.values(letterCount)) -
+    Math.min(...Object.values(letterCount))
+  );
+};
 
 testSolution("1588", part1, testFile);
 testSolution("3095", part1, inputFile);
 
-// testSolution("?", part2, testFile);
-// testSolution("?", part2, inputFile);
+testSolution("2188189693529", part2, testFile);
+testSolution("?", part2, inputFile);
