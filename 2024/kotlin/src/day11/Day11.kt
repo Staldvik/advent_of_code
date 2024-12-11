c403e5e3d677d36b99d12f51438ae36da6ce3d27
Staldvik <aasmund.staldvik@gmail.com>
2024-12-11 15:56:08 +0100

    coroutines was not really the fix, remembering to store intermediate results was..

--- 2024/kotlin/src/day11/Day11.kt
+++ 2024/kotlin/src/day11/Day11.kt
@@ -1,14 +1,10 @@
 package day11
 
-import kotlinx.coroutines.*
 import println
 import readInput
-import java.util.*
-import kotlin.time.TimeSource
 
 fun main() {
     val part1Expected = 55312
-    val part2Expected = 55312L
 
     fun applyRule(stone: Long): List<Long> {
         return when {
@@ -47,31 +43,22 @@ fun main() {
      * Start with simple recursion on one stone, 75 depth can't be that bad? This will almost be some kind of DFS then I guess
      */
 
-    val stoneAmountFromDepth = Collections.synchronizedMap(mutableMapOf<Pair<Long, Int>, Long>())
+    val stoneAmountFromDepth = mutableMapOf<Pair<Long, Int>, Long>()
 
-    suspend fun look(stone: Long, depth: Int): Long {
+    fun look(stone: Long, depth: Int): Long {
         if (depth == 0) return 1
+
         val storedResult = stoneAmountFromDepth[Pair(stone, depth)]
         if (storedResult != null) return storedResult
 
-        val newStones = applyRule(stone)
+        val result = applyRule(stone).sumOf { look(it, depth - 1) }
+        stoneAmountFromDepth[Pair(stone, depth)] = result
 
-        return coroutineScope {
-            val dispatcher = Dispatchers.Default.limitedParallelism(20)
-
-            newStones.map { newStone ->
-                async(dispatcher) {
-                    val result = look(newStone, depth - 1)
-                    stoneAmountFromDepth.putIfAbsent(Pair(newStone, depth - 1), result)
-                    result
-                }
-            }.awaitAll().sum()
-        }
+        return result
     }
 
-    suspend fun part2(input: List<String>): Long {
-        val stones = LinkedList(input.first().split(" ").map { it.toLong() })
-
+    fun part2(input: List<String>): Long {
+        val stones = input.first().split(" ").map { it.toLong() }
         return stones.sumOf { look(it, 75) }
     }
 
@@ -79,19 +66,9 @@ fun main() {
     val testInput = readInput("test")
     val part1Result = part1(testInput)
     check(part1Result == part1Expected) { "expected $part1Expected, got $part1Result" }
-    val mark = TimeSource.Monotonic.markNow()
-    val part2Result = runBlocking {
-        part2(testInput)
-    }
-    println("Execution time: ${mark.elapsedNow()}")
-    //check(part2Result == part2Expected) { "expected $part2Expected, got $part2Result" }
-
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("input")
     part1(input).println("part1")
-    val part2RealResult = runBlocking {
-        part2(input)
-    }
-    part2RealResult.println("part2")
+    part2(input).println("part2")
 }
