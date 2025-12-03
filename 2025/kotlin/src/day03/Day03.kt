3f787445014c4170d510ce4b844fbb966e61c5cf
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2025-12-03 20:52:27 +0100

    add extension func

--- 2025/kotlin/src/day03/Day03.kt
+++ 2025/kotlin/src/day03/Day03.kt
@@ -3,16 +3,15 @@ package day03
 import utils.parseInput
 
 
+fun List<Int>.maxBetween(afterIndex: Int, untilIndex: Int): Pair<Int, Int> {
+    val slice = this.slice(afterIndex until untilIndex)
+    val (index, max) = slice.withIndex().maxBy { it.value }
+    return Pair(max, index + afterIndex)
+}
+
 fun main() {
     val part1Expected = 357L
     val part2Expected = 3121910778619
-
-    fun biggestAfter(list: List<Int>, afterIndex: Int, untilIndex: Int): Pair<Int, Int> {
-        val slice = list.slice(afterIndex until untilIndex)
-        val max = slice.maxOrNull() ?: return Pair(-1, 0)
-        val index = slice.indexOfFirst { it == max }
-        return Pair(max, index + afterIndex)
-    }
 
     fun parse(input: List<String>): List<List<Int>> {
         return input.map { line -> line.split("").map { ch -> ch.toInt() } }
@@ -21,8 +20,8 @@ fun main() {
     fun part1(input: List<String>): Long {
         val banks = parse(input)
         return banks.sumOf { bank ->
-            val (first, firstIndex) = biggestAfter(bank, 0, bank.size - 1)
-            val (second, secondIndex) = biggestAfter(bank, firstIndex + 1, bank.size)
+            val (first, firstIndex) = bank.maxBetween(0, bank.size - 1)
+            val (second, secondIndex) = bank.maxBetween(firstIndex + 1, bank.size)
             "$first$second".toLong()
         }
     }
@@ -30,12 +29,12 @@ fun main() {
     fun part2(input: List<String>): Long {
         val banks = parse(input)
         return banks.sumOf { bank ->
-            val (first, firstIndex) = biggestAfter(bank, 0, bank.size - 12)
+            val (first, firstIndex) = bank.maxBetween(0, bank.size - 12)
             var num = "$first"
 
             var prevIndex = firstIndex
             while (num.length < 12) {
-                val (next, nextIndex) = biggestAfter(bank, prevIndex + 1, bank.size - (12 - num.length - 1))
+                val (next, nextIndex) = bank.maxBetween(prevIndex + 1, bank.size - (12 - num.length - 1))
                 num += next
                 prevIndex = nextIndex
             }
