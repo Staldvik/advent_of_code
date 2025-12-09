f841d8bbed8386770e3ca18a929bf1123166808a
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2025-12-09 21:35:13 +0100

    update template

--- 2025/kotlin/src/template/DayXX.kt
+++ 2025/kotlin/src/template/DayXX.kt
@@ -1,8 +1,7 @@
-package template
+package day08
 
 import utils.parseInput
 import utils.println
-
 
 fun main() {
     val part1Expected = 1
@@ -16,17 +15,20 @@ fun main() {
         return 1
     }
 
+    fun testSolution(solver: (List<String>) -> Int, input: List<String>, expected: Int) {
+        val result = solver(input)
+        check(result == expected) { "Expected $expected but got $result" }
+    }
+
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = parseInput("test")
-    val part1Result = part1(testInput)
-    check(part1Result == part1Expected) { "expected $part1Expected, got $part1Result" }
+    val testInput = parseInput("example1")
+    testInput.println("Read testinput")
+    testSolution(::part1, testInput, part1Expected)
+    //testSolution(::part2, testInput, part2Expected)
 
     // Read the input from the `src/Day01.txt` file.
     val input = parseInput("input")
-    part1(input).println("part1")
-
-    val part2Result = part2(testInput)
-    check(part2Result == part2Expected) { "expected $part2Expected, got $part2Result" }
-
-    part2(input).println("part2")
+    input.println("Read input")
+    //testSolution(::part1, input, 1)
+    //testSolution(::part2, input, 2)
 }
